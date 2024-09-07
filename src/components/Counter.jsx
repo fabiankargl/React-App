4ede492244871a71be0011ea5b035615e8c488d4
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-07 20:26:13 +0200

    logic and ui for counter limits

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
+  const [counterLimit, setCounterLimit] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   const handleIncrease = () => {
     setCounter(counter + 1);
@@ -13,16 +15,46 @@ export default function Counter() {
 
   const resetCounter = () => {
     setCounter(0);
+    setInputValue("")
   };
+
+  const handleChange = (event) => {
+    setCounterLimit(Number(event.target.value));
+    setInputValue(event.target.value);
+  };
+
+  const handleEnterPress = (event) => {
+    if (event.key === "Enter") {
+        setInputValue("");
+    }
+  };
+
   return (
     <>
       <div className="container">
-        <button onClick={handleDecrease} disabled={counter <= -10 ? true: false}>-</button>
+        <button
+          onClick={handleDecrease}
+          disabled={counter <= -counterLimit ? true : false}
+        >
+          -
+        </button>
         <span>{counter}</span>
-        <button onClick={handleIncrease} disabled={counter >= 10 ? true: false}>+</button>
+        <button
+          onClick={handleIncrease}
+          disabled={counter >= counterLimit ? true : false}
+        >
+          +
+        </button>
       </div>
       <div className="container">
         <button onClick={resetCounter}>Reset</button>
+        <label htmlFor="text">Counter limit:</label>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(event) => handleChange(event)}
+          onKeyDown={handleEnterPress}
+        />
       </div>
     </>
   );

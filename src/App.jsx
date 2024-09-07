19b1fb6d0078c824bc359846d5b0c70e1a38dde1
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-07 19:48:23 +0200

    added counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrease = () => {
+    setCounter(counter+1);
+  };
+
+  const handleDecrease = () => {
+    setCounter(counter-1);
+  }
   return (
     <>
       <div className="container">
-        <button>-</button><span>test</span><button>+</button>
+        <button onClick={handleDecrease}>-</button>
+        <span>{counter}</span>
+        <button onClick={handleIncrease}>+</button>
       </div>
     </>
   );

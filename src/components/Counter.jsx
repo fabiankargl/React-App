73a2dda1e88db051e2b4476a585f420556282ead
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-12 20:40:32 +0200

    removed counter limit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,35 +3,23 @@ import { useState } from "react";
 export default function Counter() {
   const [counterState, setCounterState] = useState({
     counter: 0,
-    counterLimit: 0,
-    inputValue: "",
     counterSteps: 1,
   });
 
   const handleIncrease = () => {
     setCounterState((prevState) => {
-      const { counter, counterSteps, counterLimit } = prevState;
-      const newCounter =
-        counter + counterSteps < counterLimit
-          ? counter + counterSteps
-          : counterLimit;
       return {
         ...prevState,
-        counter: newCounter,
+        counter: prevState.counter + counterState.counterSteps,
       };
     });
   };
 
   const handleDecrease = () => {
     setCounterState((prevState) => {
-      const { counter, counterSteps, counterLimit } = prevState;
-      const newCounter =
-        counter - counterSteps > -counterLimit
-          ? counter - counterSteps
-          : -counterLimit;
       return {
         ...prevState,
-        counter: newCounter,
+        counter: prevState.counter - counterState.counterSteps,
       };
     });
   };
@@ -41,30 +29,8 @@ export default function Counter() {
       return {
         ...prevState,
         counter: 0,
-        counterLimit: 0,
       };
     });
-  };
-
-  const handleChange = (event) => {
-    setCounterState((prevState) => {
-      return {
-        ...prevState,
-        counterLimit: Number(event.target.value),
-        inputValue: event.target.value,
-      };
-    });
-  };
-
-  const handleEnterPress = (event) => {
-    if (event.key === "Enter") {
-      setCounterState((prevState) => {
-        return {
-          ...prevState,
-          inputValue: "",
-        };
-      });
-    }
   };
 
   const handleSteps = (event) => {
@@ -78,9 +44,18 @@ export default function Counter() {
 
   return (
     <>
-    <div className="container">
-        <p>Current limit: {counterState.counterLimit}</p>
-    </div>
+      <div className="container">
+        <label htmlFor="counterSteps">Counter steps:</label>
+        <input
+          type="range"
+          name="counterSteps"
+          min="1"
+          max="10"
+          value={counterState.counterSteps}
+          onChange={(event) => handleSteps(event)}
+        ></input>
+        {counterState.counterSteps}
+      </div>
       <div className="container">
         <button
           onClick={handleDecrease}
@@ -99,27 +74,7 @@ export default function Counter() {
         >
           +
         </button>
-      </div>
-      <div className="container">
         <button onClick={resetCounter}>Reset</button>
-        <label htmlFor="counterLimit">Counter limit:</label>
-        <input
-          type="text"
-          name="counterLimit"
-          value={counterState.inputValue}
-          onChange={(event) => handleChange(event)}
-          onKeyDown={handleEnterPress}
-        />
-        <label htmlFor="counterSteps">Counter steps:</label>
-        <input
-          type="range"
-          name="counterSteps"
-          min="1"
-          max="10"
-          value={counterState.counterSteps}
-          onChange={(event) => handleSteps(event)}
-        ></input>
-        {counterState.counterSteps}
       </div>
     </>
   );

2f1fcf4fcc5b4bd0ad2b2e7da702f0c734639806
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-12 21:35:51 +0200

    show counter steps conditionally and styled more

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,13 +4,16 @@ export default function Counter() {
   const [counterState, setCounterState] = useState({
     counter: 0,
     counterSteps: 1,
+    stepsOnOff: false,
   });
 
   const handleIncrease = () => {
     setCounterState((prevState) => {
       return {
         ...prevState,
-        counter: prevState.counter + counterState.counterSteps,
+        counter: prevState.stepsOnOff
+          ? prevState.counter + prevState.counterSteps
+          : prevState.counter + 1,
       };
     });
   };
@@ -19,7 +22,9 @@ export default function Counter() {
     setCounterState((prevState) => {
       return {
         ...prevState,
-        counter: prevState.counter - counterState.counterSteps,
+        counter: prevState.stepsOnOff
+          ? prevState.counter - prevState.counterSteps
+          : prevState.counter - 1,
       };
     });
   };
@@ -42,23 +47,18 @@ export default function Counter() {
     });
   };
 
+  const handleStepsOnOff = () => {
+    setCounterState((prevState) => {
+      return {
+        ...prevState,
+        stepsOnOff: !prevState.stepsOnOff,
+      };
+    });
+  };
+
   return (
     <div className="flex flex-col items-center bg-slate-300 m-4 rounded-3xl p-4 w-1/2 mx-auto">
       <h1 className="text-2xl font-bold text-blue-600">Counter</h1>
-      <div className="flex flex-col items-center my-5">
-        <label htmlFor="counterSteps" className="text-sm mb-2">
-          Counter steps:
-        </label>
-        <input
-          type="range"
-          name="counterSteps"
-          min="1"
-          max="10"
-          value={counterState.counterSteps}
-          onChange={(event) => handleSteps(event)}
-          className=" my-2"
-        />
-      </div>
       <div className="flex items-center justify-center my-5">
         <button
           onClick={handleDecrease}
@@ -76,14 +76,40 @@ export default function Counter() {
           +
         </button>
       </div>
-      <div className="flex items-center flex-col">
+      <div className="flex items-center">
         <button
           onClick={resetCounter}
           className="text-sm mx-1 py-1 px-2 bg-slate-600 rounded text-yellow-50"
         >
           Reset
         </button>
+        <input
+          type="checkbox"
+          id="steps"
+          className="form-checkbox h-5 w-5 text-blue-600 mx-2"
+          onClick={handleStepsOnOff}
+        />
+        <label htmlFor="steps" className="text-lg">
+          Counter steps
+        </label>
       </div>
+      {counterState.stepsOnOff && (
+        <div className="flex items-center  m-5">
+          <label htmlFor="counterSteps" className="text-lg text-gray-700 mr-4">
+            Steps:
+          </label>
+          <input
+            type="range"
+            name="counterSteps"
+            min="1"
+            max="10"
+            value={counterState.counterSteps}
+            onChange={(event) => handleSteps(event)}
+            className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg"
+          />
+          <span className="text-lg ml-4">{counterState.counterSteps}</span>
+        </div>
+      )}
     </div>
   );
 }

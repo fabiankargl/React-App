5cd1ca6117e3f25e038d3aa5471114ced7facffd
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-14 21:55:28 +0200

    added testid and test for toDoList

---
+++ src/e2e/TooList.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { act } from 'react';
+import ToDoList from '../components/ToDoList';
+
+test('Render start items', () => {
+    render(<ToDoList />);
+
+    const input = screen.getByTestId("todo_input");
+    expect(input).toBeInTheDocument();
+
+    const buttonAdd = screen.getByTestId("todo_btn_add");
+    expect(buttonAdd).toBeInTheDocument();
+
+    act(() => {
+        fireEvent.change(input, { target: { value: 'New ToDo Item' } });
+        buttonAdd.click();
+    });
+
+    const todoElement = screen.getByTestId("todo_element");
+    expect(todoElement).toBeInTheDocument();
+
+    const buttonDelete = screen.getByTestId("todo_btn_delete");
+    expect(buttonDelete).toBeInTheDocument();
+});

4bedecde4043b50c751c5d0bfbf819f875546d3d
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-14 16:58:50 +0200

    added delete function and fixed localStorage issues

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 export default function ToDoList() {
   const [toDoList, setToDoList] = useState([]);
   const [toDo, setToDo] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
   const handleInput = (event) => {
     setToDo(event.target.value);
@@ -22,24 +23,34 @@ export default function ToDoList() {
     setToDo("");
   };
 
+  const deleteToDo = (id) => {
+    setToDoList((prevToDoList) => {
+      const filteredToDoList = prevToDoList.filter((toDo) => toDo.id !== id);
+      return filteredToDoList;
+    });
+  };
+
   useEffect(() => {
     const storedToDoList = localStorage.getItem("toDoList");
     if (storedToDoList) {
       setToDoList(JSON.parse(storedToDoList));
     }
+    setLoaded(true); 
   }, []);
 
   useEffect(() => {
-    if (toDoList.length > 0) {
+    if (loaded) {
       localStorage.setItem("toDoList", JSON.stringify(toDoList));
     }
-  }, [toDoList]);
+  }, [toDoList, loaded]);
 
   return (
     <div className="flex flex-col items-center bg-slate-300 m-4 rounded-3xl p-4 w-1/2 mx-auto">
       <h1 className="text-2xl font-bold text-blue-600">To-Do-List</h1>
       <div className="flex m-2 justify-between space-x-3">
-        <label htmlFor="todo" className="text-lg">Add ToDo:</label>
+        <label htmlFor="todo" className="text-lg">
+          Add ToDo:
+        </label>
         <input
           type="text"
           id="todo"
@@ -53,14 +64,20 @@ export default function ToDoList() {
           Add
         </button>
       </div>
-      <ul className="m-2 p-2  w-1/2">
+      <ul className="m-2 p-2 w-1/2">
         {toDoList.map((todo) => {
           return (
             <li
               key={todo.id}
-              className="p-2 border border-gray-400 rounded-lg text-stone-800 text-xl mt-1  w-full"
+              className="flex justify-between items-center p-2 border border-gray-400 rounded-lg text-stone-800 text-xl mt-1  w-full"
             >
-              {todo.toDo}
+              <span>{todo.toDo}</span>
+              <button
+                className="text-sm px-2 py-1 bg-red-500 text-white rounded"
+                onClick={() => deleteToDo(todo.id)}
+              >
+                Delete
+              </button>
             </li>
           );
         })}

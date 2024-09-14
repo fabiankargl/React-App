949d7705106939f26e74734dab2354f3be15977f
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-14 21:18:22 +0200

    added testIds and deleted App.test file

--- src/components/UI/Label.jsx
+++ src/components/UI/Label.jsx
@@ -1,8 +1,9 @@
-export const Label = ({ children, forHtml, className }) => {
+export const Label = ({ children, forHtml, className, testId }) => {
     return (
       <label
         htmlFor={forHtml}
         className={`text-lg p-2 ${className}`}
+        testId={testId}
       >
         {children}
       </label>

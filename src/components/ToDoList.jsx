285b7e00ee9fd16bbf5d6b9e8047b123975f0b94
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-14 16:45:18 +0200

    toDo saved in localStorage

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function ToDoList() {
   const [toDoList, setToDoList] = useState([]);
@@ -9,6 +9,7 @@ export default function ToDoList() {
   };
 
   const addToDo = (toDo) => {
+    if (toDo.trim() === "") return;
     setToDoList((prevToDoList) => {
       return [
         ...prevToDoList,
@@ -18,14 +19,33 @@ export default function ToDoList() {
         },
       ];
     });
+    setToDo("");
   };
+
+  useEffect(() => {
+    const storedToDoList = localStorage.getItem("toDoList");
+    if (storedToDoList) {
+      setToDoList(JSON.parse(storedToDoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (toDoList.length > 0) {
+      localStorage.setItem("toDoList", JSON.stringify(toDoList));
+    }
+  }, [toDoList]);
 
   return (
     <div className="flex flex-col items-center bg-slate-300 m-4 rounded-3xl p-4 w-1/2 mx-auto">
       <h1 className="text-2xl font-bold text-blue-600">To-Do-List</h1>
       <div className="flex m-2 justify-between space-x-3">
-        <label htmlFor="todo">Add ToDo:</label>
-        <input type="text" id="todo" onChange={(event) => handleInput(event)} />
+        <label htmlFor="todo" className="text-lg">Add ToDo:</label>
+        <input
+          type="text"
+          id="todo"
+          value={toDo}
+          onChange={(event) => handleInput(event)}
+        />
         <button
           className="text-sm px-2 py-1 mx-1 bg-blue-500 text-white rounded disabled:bg-gray-300"
           onClick={() => addToDo(toDo)}
@@ -33,10 +53,17 @@ export default function ToDoList() {
           Add
         </button>
       </div>
-      <ul className="m-4 p-4  w-1/2">
-        <li className="p-2 border border-gray-400 rounded-lg text-stone-800 text-xl  w-full">
-          tets
-        </li>
+      <ul className="m-2 p-2  w-1/2">
+        {toDoList.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              className="p-2 border border-gray-400 rounded-lg text-stone-800 text-xl mt-1  w-full"
+            >
+              {todo.toDo}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );

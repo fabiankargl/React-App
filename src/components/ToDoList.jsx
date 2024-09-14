64e43b6a68480d96f5d3bc66f61c22c73f7dc1e3
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-14 16:22:38 +0200

    structure for toDoList

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,17 @@
+export default function ToDoList() {
+  return (
+    <div className="flex flex-col items-center bg-slate-300 m-4 rounded-3xl p-4 w-1/2 mx-auto">
+      <h1 className="text-2xl font-bold text-blue-600">To-Do-List</h1>
+      <div className="flex m-2 justify-between space-x-3">
+        <label htmlFor="todo">Add ToDo:</label>
+        <input type="text" id="todo" />
+        <button className="text-sm px-2 py-1 mx-1 bg-blue-500 text-white rounded disabled:bg-gray-300">
+          Add
+        </button>
+      </div>
+      <ul className="m-4 p-4  w-1/2">
+        <li className="p-2 border border-gray-400 rounded-lg text-stone-800 text-xl  w-full">tets</li>
+      </ul>
+    </div>
+  );
+}

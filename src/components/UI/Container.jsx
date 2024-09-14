a60e7a288c7104912b3abe10e51cfb7b680c0b77
Fabian Kargl <mt221024@fhstp.ac.at>
2024-09-14 22:04:07 +0200

    added hovering to container

--- src/components/UI/Container.jsx
+++ src/components/UI/Container.jsx
@@ -1,6 +1,6 @@
 export const Container = ({ children }) => {
   return (
-    <div className="flex flex-col items-center bg-slate-300 m-4 rounded-3xl p-4 w-1/2 mx-auto">
+    <div className="flex flex-col items-center bg-slate-300 m-4 rounded-3xl p-4 w-1/2 mx-auto hover:shadow-lg hover:shadow-blue-500/50">
       {children}
     </div>
   );

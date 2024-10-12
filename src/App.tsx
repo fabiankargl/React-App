b8a24b80acace96c863884387659ccf813dee4ca
Fabian Kargl <mt221024@fhstp.ac.at>
2024-10-12 21:14:21 +0200

    added basic dashboard strcuture and dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Dashboard } from "./main/components/Dashboard/Dashboard";
 import { MainMenu } from "./main/components/NavBar/Menu";
+
 
 const App = () => {
   return (
     <>
       <MainMenu />
+      <Dashboard />
     </>
   );
 };
